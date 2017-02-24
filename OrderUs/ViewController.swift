530210148faf7b38eabf174d381ae542807ecc84
muaz hamza <muazhamza@muazs-MacBook-Air.local>
2017-02-24 18:27:41 +0500

    Initial Commit

---
+++ OrderUs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OrderUs
+//
+//  Created by muaz hamza on 2017-02-24.
+//  Copyright © 2017 PRO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
